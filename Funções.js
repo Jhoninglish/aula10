6239b1a64744da0dd446aa943f6acbdb3fb2e799
Jhoninglish <142617436+Jhoninglish@users.noreply.github.com>
2023-08-30 17:21:18 -0300

    Create Funções.js

---
+++ Funções.js
@@ -0,0 +1,50 @@
+// this is a js file
+// João Vítor da Silva Rosa
+/*
+function escreverNome(nome){
+    console.log(nome);
+}
+escreverNome("João Vítor");
+
+const a = 1
+
+function imprimirVariavel () {
+const b = 2
+ console.log('Variavel a', a)
+ console.log('Variavel b', b)
+}
+
+imprimeVariavel()
+
+console.log('Variável a', a)
+console.log('Variavel b', b)
+
+function calculaArea(altura, largura) {
+    const area = altura * largura;
+    return area;
+}
+const areaCalculada = calculaArea(2, 3);
+console.log(calculaArea(2, 3));
+
+function somaValores(a,b){
+    return Number(a)+Number(b);
+}
+const resultadoDaSoma = somaValores(2,4);
+console.log("O resultado da soma é: " + resultadoDaSoma);
+
+let texto = prompt("Digite um texto");
+function Texto() {
+    return texto;
+}
+console.log(texto);
+
+function divideArray(meuArray){
+let b = [(meuArray[0] /2),(meuArray[meuArray.length-1] /2)];
+
+return b;
+}
+let a = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
+
+const resultado = divideArray(a)
+console.log(resultado);
+*/
